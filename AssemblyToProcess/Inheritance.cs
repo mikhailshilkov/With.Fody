98dd3bf3dbae66e69272500324654cfc257e7a18
Antao Almada <antao.almada@sky.uk>
2017-03-06 14:38:09 +0000

    Added support for inheritance (some properties defined on base class level) (#7)

---
+++ AssemblyToProcess/Inheritance.cs
@@ -0,0 +1,30 @@
+﻿namespace AssemblyToProcess
+{
+    public class BaseInheritance
+    {
+        public BaseInheritance(int value1, string value2)
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+        }
+
+        public int Value1 { get; }
+
+        public string Value2 { get; }
+
+        public BaseInheritance With<T>(T value) => this;
+    }
+
+    public class Inheritance : BaseInheritance
+    {
+        public Inheritance(int value1, string value2, long value3)
+            : base(value1, value2)
+        {
+            this.Value3 = value3;
+        }
+
+        public long Value3 { get; }
+
+        public new Inheritance With<T>(T value) => this;
+    }
+}
