94ae5c59eec2f86c5e95ccf2e1cf77ab85742ae7
mikhailshilkov <mshilkov@astrata.eu>
2017-03-08 12:54:26 +0100

    Bump nuget version

--- With.Fody/AssemblyInfo.cs
+++ With.Fody/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("With.Fody")]
 [assembly: AssemblyProduct("With.Fody")]
-[assembly: AssemblyVersion("0.5.1")]
-[assembly: AssemblyFileVersion("0.5.1")]
+[assembly: AssemblyVersion("0.6.0")]
+[assembly: AssemblyFileVersion("0.6.0")]
