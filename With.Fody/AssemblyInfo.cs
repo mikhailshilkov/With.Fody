e5e0877bc85841081ec0d1647f7a2f0744d165db
Antao Almada <antao.almada@gmail.com>
2019-06-15 19:42:54 +0100

    Upgraded to Fody 5.0.6 and to netstandard 2.0

--- With.Fody/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("With.Fody")]
-[assembly: AssemblyProduct("With.Fody")]
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
